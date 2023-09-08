680d718293e8042527baa2c40b487c3fb15868e4
mdyasinbhuiyan112233@gmail.com <mdyasinbhuiyan112233@gmail.com>
2023-09-08 16:39:16 +0600

    HTML

---
+++ 12_js/script.js
@@ -0,0 +1,12 @@
+  
+//js can change content
+function changetitle() {
+  document.getElementById('title').innerHTML = 'Use JavaScript in HTML Page';
+}
+  
+//js can change style
+  function changePara() {
+  document.getElementById('para').style.color = '#707070';
+  document.getElementById('para').style.lineHeight = '25px';
+  document.getElementById('para').style.fontFamily = 'sans-serif';
+}
