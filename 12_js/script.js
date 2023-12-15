49b9228aa3997cd28a9c8ab90616dc215c8ed3b7
mdyasinbhuiyan112233@gmail.com <mdyasinbhuiyan112233@gmail.com>
2023-12-15 11:46:23 +0600

    html is ok

--- 12_js/script.js
+++ 12_js/script.js
@@ -1,12 +1,24 @@
-  
 //js can change content
 function changetitle() {
   document.getElementById('title').innerHTML = 'Use JavaScript in HTML Page';
 }
   
-//js can change style
-  function changePara() {
+/* //js can change style
+ function changePara() {   
   document.getElementById('para').style.color = '#707070';
   document.getElementById('para').style.lineHeight = '25px';
   document.getElementById('para').style.fontFamily = 'sans-serif';
+} */
+
+/* //js can change style
+function changePara(){
+  const para = document.getElementById('para');
+  para.style.color = '#707070';
+  para.style.lineHeight = '25px';
+  para.style.fontFamily = 'sans-serif';
+} */
+
+function changePara(){
+  const para = document.getElementById('para');
+  para.style = 'color: #707070; line-height: 25px; font-family: sans-serif;';
 }
